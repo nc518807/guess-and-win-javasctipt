74a4537351d04f6dfe62f32b16720b49be73697e
nc518807 <nc518807@gmail.com>
2022-03-13 22:25:57 +0545

    final

--- game.js
+++ game.js
@@ -1,44 +1,59 @@
 const msg = document.querySelector('.msg');
 const guess = document.querySelector('input');
 const btn = document.querySelector('.btn');
-let newWords ="";
-let randomWords="";
+let newWords = "";
+let randomWords = "";
 let play = false;
-let sWords = ['python','java','laravel','android','c++','swift','angular','sql','reactjs'];
+let sWords = ['python', 'java', 'laravel', 'android', 'c++', 'swift', 'angular', 'sql', 'reactjs'];
 
- const createNewwords= ()=>{
-     let ranNum = Math.floor(Math.random() * sWords.length );
- 
-     //console.log(ranNum);
-     let newTempWords = sWords[ranNum];
-     //console.log(newTempWords.split(""));
-     return newTempWords;
- }
- const Scramblewords = (arr)=>{
-    for(let i = arr.length-1; i>0; i--)
-{
-    let temp = arr[i];
-    //console.log(temp);
-    let j = Math.floor(Math.random()* (i+1));
-    //.log(j);
-    arr[i] =arr[j];
-    arr[j]= temp;
+const createNewwords = () => {
+    let ranNum = Math.floor(Math.random() * sWords.length);
 
-     
-}    
-return arr;
+    //console.log(ranNum);
+    let newTempWords = sWords[ranNum];
+    //console.log(newTempWords.split(""));
+    return newTempWords;
+}
+const Scramblewords = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let temp = arr[i];
+        //console.log(temp);
+        let j = Math.floor(Math.random() * (i + 1));
+        //.log(j);
+        arr[i] = arr[j];
+        arr[j] = temp;
 
- }
 
-btn.addEventListener('click',function(){
-    if(!play){
-        play= true;
-        btn.innerHTML= "Guess";
+    }
+    return arr;
+
+}
+
+btn.addEventListener('click', function () {
+    if (!play) {
+        play = true;
+        btn.innerHTML = "Guess";
         guess.classList.toggle('hidden');
-      newWords =  createNewwords(); 
-      randomWords = Scramblewords(newWords.split("")).join("");
-     // console.log(randomWords.join());
-     msg.innerHTML = randomWords;
+        newWords = createNewwords();
+        randomWords = Scramblewords(newWords.split("")).join("");
+        // console.log(randomWords.join());
+        msg.innerHTML = ` guess th value budddy:${randomWords}`;
+    }
+    else {
+        let tempWords = guess.value;
+        if (tempWords === newWords) {
+            //console.log('correct');
+            play= false;
+            
+            msg.innerHTML=`great!! you are correct ${newWords}`;
+           btn.innerHTML= "start Again";
+           guess.classList.toggle('hidden');
+           guess.value = "";
+        } else {
+            
+            msg.innerHTML= `oh no!! better luck ${randomWords}`;
+            //console.log('incorrect');
+        }
     }
 
 })

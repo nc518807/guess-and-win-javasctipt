6f688fd0ddeb2b14690ed40f1135fb6497d56a8b
nc518807 <nc518807@gmail.com>
2022-03-13 18:08:12 +0545

    before shuffling the word

--- game.js
+++ game.js
@@ -1,17 +1,30 @@
 const msg = document.querySelector('.msg');
 const guess = document.querySelector('input');
 const btn = document.querySelector('.btn');
+let newWords ="";
+let randomWords="";
 let play = false;
 let sWords = ['python','java','laravel','android','c++','swift','angular','sql','reactjs'];
 
  const createNewwords= ()=>{
-     
+     let ranNum = Math.floor(Math.random() * sWords.length );
+ 
+     //console.log(ranNum);
+     let newTempWords = sWords[ranNum];
+     console.log(newTempWords.split(""));
+     return newTempWords;
  }
+ const Scramblewords = ()=>{
+
+ }
+
 btn.addEventListener('click',function(){
     if(!play){
         play= true;
         btn.innerHTML= "Guess";
         guess.classList.toggle('hidden');
+      newWords =  createNewwords();
+      randomWords = Scramblewords(newWords);
     }
 
 })
